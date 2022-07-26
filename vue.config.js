5c1725a4d3735e1bf0c67ec254113504e06820ab
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-07-26 12:42:40 +0300

    init

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   lintOnSave: false,
+  publicPath: process.env.NODE_ENV === "production" ? "/idaproject/" : "/",
+  css: {
+    loaderOptions: {
+      sass: {
+        // additionalData: `
+        // 		@import "@/styles/_imports.scss";
+        // 	`,
+      },
+    },
+  },
 };
