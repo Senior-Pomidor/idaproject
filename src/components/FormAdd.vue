ad13e3a9a2d7bb50256dd9ce94ebb94c482f7ba3
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-07-28 12:19:16 +0300

    added all styles for FormAdd component

--- src/components/FormAdd.vue
+++ src/components/FormAdd.vue
@@ -1,73 +1,74 @@
 <template>
-  <form action="" :class="formAdd['form-add']">
-    <div :class="{ [formAdd.group]: true, [formAdd.required]: true }">
-      <label :class="formAdd.label" for="title">Наименование товара</label>
-      <input
-        :class="formAdd.input"
-        type="text"
-        id="title"
-        name="title"
-        placeholder="Введите наименование товара"
-      />
-      <small :class="formAdd['required-helper']">
-        Поле является обязательным
-      </small>
-    </div>
+	<form action="" :class="formAdd['form-add']">
+		<div :class="{ [formAdd.group]: true, [formAdd.required]: true }">
+			<label :class="formAdd.label" for="title">Наименование товара</label>
+			<input
+				:class="{[formAdd.input] : true, [formAdd['validation-required']] : true}"
+				type="text"
+				id="title"
+				name="title"
+				placeholder="Введите наименование товара"
+			/>
+			<small :class="formAdd['required-helper']">
+				Поле является обязательным
+			</small>
+		</div>
 
-    <div :class="formAdd.group">
-      <label :class="formAdd.label" for="descripton">
-        Описание товара
-      </label>
-      <textarea
-        :class="{ [formAdd.input]: true, [formAdd.textarea]: true }"
-        id="descripton"
-        name="descripton"
-        placeholder="Введите описание товара"
-      ></textarea>
-    </div>
+		<div :class="formAdd.group">
+			<label :class="formAdd.label" for="descripton">
+				Описание товара
+			</label>
+			<textarea
+				:class="{ [formAdd.input]: true, [formAdd.textarea]: true }"
+				rows="5"
+				id="descripton"
+				name="descripton"
+				placeholder="Введите описание товара"
+			></textarea>
+		</div>
 
-    <div :class="{ [formAdd.group]: true, [formAdd.required]: true }">
-      <label :class="formAdd.label" for="img">
-        Ссылка на изображение товара
-      </label>
-      <input
-        :class="formAdd.input"
-        type="text"
-        id="img"
-        name="img"
-        placeholder="Введите ссылку"
-      />
-      <small :class="formAdd['required-helper']">
-        Поле является обязательным
-      </small>
-    </div>
+		<div :class="{ [formAdd.group]: true, [formAdd.required]: true }">
+			<label :class="formAdd.label" for="img">
+				Ссылка на изображение товара
+			</label>
+			<input
+				:class="{[formAdd.input] : true, [formAdd['validation-required']] : true}"
+				type="text"
+				id="img"
+				name="img"
+				placeholder="Введите ссылку"
+			/>
+			<small :class="formAdd['required-helper']">
+				Поле является обязательным
+			</small>
+		</div>
 
-    <div :class="{ [formAdd.group]: true, [formAdd.required]: true }">
-      <label :class="formAdd.label" for="price"> Цена товара </label>
-      <input
-        :class="formAdd.input"
-        type="text"
-        id="price"
-        name="price"
-        placeholder="Введите цену"
-      />
-      <small :class="formAdd['required-helper']">
-        Поле является обязательным
-      </small>
-    </div>
+		<div :class="{ [formAdd.group]: true, [formAdd.required]: true }">
+			<label :class="formAdd.label" for="price"> Цена товара </label>
+			<input
+				:class="{[formAdd.input] : true, [formAdd['validation-required']] : true}"
+				type="text"
+				id="price"
+				name="price"
+				placeholder="Введите цену"
+			/>
+			<small :class="formAdd['required-helper']">
+				Поле является обязательным
+			</small>
+		</div>
 
-    <input
-      :class="{ [formAdd['submit-btn']]: true, [formAdd.input]: true }"
-      type="submit"
-      value="Добавить товар"
-      @click.prevent
-    />
-  </form>
+		<input
+			:class="{ [formAdd['submit-btn']]: true, [formAdd.input]: true }"
+			type="submit"
+			value="Добавить товар"
+			@click.prevent
+		/>
+	</form>
 </template>
 
 <script>
 export default {
-  name: "form-add",
+	name: "form-add",
 };
 </script>
 
@@ -83,6 +84,7 @@ $color-green: #7bae73 !default;
 $font-family-dafault: Arial !default;
 $color-grey-light: #eeeeee !default;
 $color-red: #FF8484 !default;
+$color-green: #00A76B !default;
 
 // $font-color: red;
 $bg-color: $color-white;
@@ -91,6 +93,7 @@ $label-font-color: $font-color-dafault;
 $input-font-color: $color-black;
 $input-bg-color: $color-white;
 $input-placeholder-color: $color-grey;
+$input-border-width: 1px;
 
 $btn-color: $input-placeholder-color;
 $btn-bg-color: $color-grey-light;
@@ -100,97 +103,99 @@ $btn-bg-color--active: $color-green;
 $required-helper-color: $color-red;
 
 .form-add {
-  width: 100%;
-  max-width: 332px;
-  margin: 0 auto; //dev
-  // color: $font-color;
-  background-color: $bg-color;
-  border-radius: $border-radius;
-  padding: 1.5rem;
-  box-sizing: border-box;
+	width: 100%;
+	max-width: 480px;
+	background-color: $bg-color;
+	border-radius: $border-radius;
+	padding: 1.5rem;
+	box-sizing: border-box;
 
-  .group {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    width: 100%;
+	.group {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		width: 100%;
 		position: relative;
-    margin-bottom: 1rem;
-    // display: flex;
-    // flex-wrap: wrap;
-		&:last-of-type {
-			margin-bottom: 1.5rem;
+		margin-bottom: 1rem;
+		// display: flex;
+		// flex-wrap: wrap;
+		// &:last-child {
+		// 	margin-bottom: 1.5rem;
+		// }
+	}
+
+	.label {
+		display: inline-block;
+		color: $label-font-color;
+		font-size: 0.625rem;
+		font-weight: 400;
+		line-height: 1.3;
+		letter-spacing: -0.02em;
+		margin-bottom: 0.25rem;
+	}
+
+	.input {
+		position: relative;
+		width: 100%;
+
+		color: $input-font-color;
+		font-size: 0.75rem;
+		line-height: 1.25;
+
+		background-color: $input-bg-color;
+		border: none;
+		box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+		border: 1px solid transparent;
+		border-radius: $border-radius;
+		padding: calc(0.625rem - $input-border-width) calc(1rem - $input-border-width) calc(0.625rem + 1px - $input-border-width);
+		box-sizing: border-box;
+		
+		transition: border .2s ease;
+
+		&::-webkit-input-placeholder {
+			color: $input-placeholder-color;
+			font-family: $font-family-dafault, sans-serif;
 		}
-  }
 
-  .label {
-    display: inline-block;
-    color: $label-font-color;
-    font-size: 0.625rem;
-    font-weight: 400;
-    line-height: 1.3;
-    letter-spacing: -0.02em;
-    margin-bottom: 0.25rem;
-  }
+		&::-moz-placeholder,
+		&:-moz-placeholder {
+			color: $input-placeholder-color;
+			font-family: $font-family-dafault, sans-serif;
+		}
 
-  .input {
-    width: 100%;
-
-    color: $input-font-color;
-    font-size: 0.75rem;
-    line-height: 1.25;
-
-    background-color: $input-bg-color;
-    border: none;
-    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-    border-radius: $border-radius;
-    padding: 0.625rem 1rem calc(0.625rem + 1px);
-    box-sizing: border-box;
-
-    &::-webkit-input-placeholder {
-      color: $input-placeholder-color;
-      font-family: $font-family-dafault, sans-serif;
-    }
-
-    &::-moz-placeholder,
-    &:-moz-placeholder {
-      color: $input-placeholder-color;
-      font-family: $font-family-dafault, sans-serif;
-    }
-    
-    &:focus {
-      border: none;
-      outline: none;
-    }
-  }
+		&:focus {
+			border: 1px solid $color-green;
+			outline: none;
+		}
+		
+		&.validation-required {
+			&:focus {
+			border-color: $color-red;
+				+ .required-helper {
+					visibility: visible;
+					opacity: 1;
+				}
+			}
+		}
+	}
 	
 	.textarea {
 		min-height: 6.75rem;
 		resize: none;
 	}
 
-  .submit-btn {
-    display: block;
-    width: 100%;
+	.submit-btn {
+		display: block;
+		width: 100%;
 
-    text-align: center;
-    color: $btn-color;
-    font-weight: 600;
-    letter-spacing: -0.02em;
+		text-align: center;
+		color: $btn-color;
+		font-weight: 600;
+		letter-spacing: -0.02em;
 
-    background-color: $btn-bg-color;
-  }
-	
-  .required-helper {
-		position: absolute;
-		left: 0;
-		top: calc(100% + .25rem);
-		font-size: .5rem;
-		line-height: 1.25;
-		color: $required-helper-color;
-		// visibility: hidden;
-		pointer-events: none;
-  }
+		background-color: $btn-bg-color;
+		margin-top: 1.5rem;
+	}
 	
 	.required {
 		.label {
@@ -208,6 +213,20 @@ $required-helper-color: $color-red;
 				content: "";
 			}
 		}
+	}
+	
+	.required-helper {
+		position: absolute;
+		left: 0;
+		top: calc(100% + .25rem);
+		font-size: .5rem;
+		line-height: 1.25;
+		color: $required-helper-color;
+		pointer-events: none;
+		
+		visibility: hidden;
+		opacity: 0;
+		// transition: visibility .1s ease, opacity .1s ease;
 	}
 }
 </style>
