eeff466d8798fa135790a8507132f541b2ee0609
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-08-01 12:55:08 +0300

    build

--- precache-manifest.2fe531ab84f6b6332f119eb7a98b326b.js
+++ precache-manifest.2fe531ab84f6b6332f119eb7a98b326b.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fdc795a180fb1b1fd19d",
+    "revision": "ef99ed7143987b07b093",
     "url": "/idaproject/css/app.2cb04255.css"
   },
   {
-    "revision": "4a31040bf0fa1e74e916",
-    "url": "/idaproject/css/chunk-42b0c84c.e417d0d3.css"
+    "revision": "716c22075cd9c64cc023",
+    "url": "/idaproject/css/chunk-1a9db1d2.5e8a3360.css"
   },
   {
     "revision": "ff671c058f8c866e2044d7c7e8e7a689",
     "url": "/idaproject/img/content/img-card.jpg"
   },
   {
-    "revision": "b79fd8e215dd15a1234af0442e3ff7f9",
+    "revision": "64c87177381c65753d0c00fa6cdbffef",
     "url": "/idaproject/index.html"
   },
   {
-    "revision": "fdc795a180fb1b1fd19d",
-    "url": "/idaproject/js/app.cf3b7868.js"
+    "revision": "ef99ed7143987b07b093",
+    "url": "/idaproject/js/app.6df4c23d.js"
   },
   {
-    "revision": "677fd82ce120426d047c",
-    "url": "/idaproject/js/chunk-2d230cde.90677e56.js"
+    "revision": "716c22075cd9c64cc023",
+    "url": "/idaproject/js/chunk-1a9db1d2.321426d2.js"
   },
   {
-    "revision": "4a31040bf0fa1e74e916",
-    "url": "/idaproject/js/chunk-42b0c84c.d10355c4.js"
+    "revision": "9eea83d28950dc34b58b",
+    "url": "/idaproject/js/chunk-2d230cde.5f0f6d85.js"
   },
   {
     "revision": "b91879838a35cb57a5cc",
