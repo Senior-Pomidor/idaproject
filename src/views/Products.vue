e03b411156587e2345d24d051b39f9bd4b6080f2
Senior-Pomidor <bobylev.vladi@gmail.com>
2022-08-02 14:51:09 +0300

    edited layout, header and dropdown styles

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,39 +1,63 @@
 <template>
-	<main :class="products.products">
+	<div :class="products.layout">
+		<header :class="products.header">
 			<div :class="products.container" class="container">
-				<aside :class="products.aside">
-					<h2 :class="products.title">
-						Добавление товара
-					</h2>
-					<FormAdd :class="products.form" />
-				</aside>
+				<h1 :class="products.title">
+					Добавление товара
+				</h1>
 				
-				<article :class="products.content">
-					<div :class="products.filters">
-						<div :class="{[products.dropdown]: true, [products.opened]: isDropdownOpened}">
-							<button :class="products['dropdown-btn']" @click="isDropdownOpened = !isDropdownOpened">
-								<span v-html="filter"></span>
-								<i :class="products['dropdown-icon']"></i>
-							</button>
-							<ul :class="products['options']">
-								<li :class="products['option']" @click="sortByPriceUp(), isDropdownOpened = !isDropdownOpened, setOption('priceUp')" v-html="filters.priceUp"></li>
-								<li :class="products['option']" @click="sortByPriceDown(), isDropdownOpened = !isDropdownOpened, setOption('priceDown')" v-html="filters.priceDown"></li>
-								<li :class="products['option']" @click="sortByName(), isDropdownOpened = !isDropdownOpened, setOption('name')" v-html="filters.name"></li>
-							</ul>
-							
-							<!-- <input type="hidden" :name="{ name }" :value="{ value }"> -->
-						</div>
-					</div>
-					<div :class="products.cards">
-						<Card
-								v-for="product in PRODUCTS"
-								:key="product.id"
-								:info="product"
-								:class="{ [products['new-product']]: product.id == $store.state.products.newId }" />
-					</div>
-				</article>
+				<div :class="products.sort">
+							<div :class="{[products.dropdown]: true, [products.opened]: isDropdownOpened}">
+								<button :class="products['dropdown-btn']" @click="isDropdownOpened = !isDropdownOpened">
+									<span v-html="filter"></span>
+									<i :class="products['dropdown-icon']"></i>
+								</button>
+								<ul :class="products['options']">
+									<li :class="products['option']" @click="sortByPriceUp(), isDropdownOpened = !isDropdownOpened, setOption('priceUp')" v-html="filters.priceUp"></li>
+									<li :class="products['option']" @click="sortByPriceDown(), isDropdownOpened = !isDropdownOpened, setOption('priceDown')" v-html="filters.priceDown"></li>
+									<li :class="products['option']" @click="sortByName(), isDropdownOpened = !isDropdownOpened, setOption('name')" v-html="filters.name"></li>
+								</ul>
+								
+								<!-- <input type="hidden" :name="{ name }" :value="{ value }"> -->
+							</div>
+				</div>
 			</div>
-	</main>
+		</header>
+		
+		<main :class="products.products">
+				<div :class="products.container" class="container">
+					<aside :class="products.aside">
+
+						<FormAdd :class="products.form" />
+					</aside>
+					
+					<article :class="products.content">
+							<div :class="products.sort">
+										<div :class="{[products.dropdown]: true, [products.opened]: isDropdownOpened}">
+											<button :class="products['dropdown-btn']" @click="isDropdownOpened = !isDropdownOpened">
+												<span v-html="filter"></span>
+												<i :class="products['dropdown-icon']"></i>
+											</button>
+											<ul :class="products['options']">
+												<li :class="products['option']" @click="sortByPriceUp(), isDropdownOpened = !isDropdownOpened, setOption('priceUp')" v-html="filters.priceUp"></li>
+												<li :class="products['option']" @click="sortByPriceDown(), isDropdownOpened = !isDropdownOpened, setOption('priceDown')" v-html="filters.priceDown"></li>
+												<li :class="products['option']" @click="sortByName(), isDropdownOpened = !isDropdownOpened, setOption('name')" v-html="filters.name"></li>
+											</ul>
+											
+											<!-- <input type="hidden" :name="{ name }" :value="{ value }"> -->
+										</div>
+							</div>
+						<div :class="products.cards">
+							<Card
+									v-for="product in PRODUCTS"
+									:key="product.id"
+									:info="product"
+									:class="{ [products['new-product']]: product.id == $store.state.products.newId }" />
+						</div>
+					</article>
+				</div>
+		</main>
+	</div>
 </template>
 
 <script>
@@ -85,19 +109,158 @@ export default {
 <style lang="scss" module="products">
 // Дефолтные значения переменных, если не заданы глобальные
 $font-color-dafault: #3F3F3F !default;
-$container-padding: 2rem !default;
-$container-padding--mobile: 1rem !default;
+// $container-padding: 2rem !default;
+// $container-padding--mobile: 1rem !default;
 $font-family-default: Arial !default;
 $color-white: #FFFEFB !default;
 $color-grey: #B4B4B4 !default;
 $color-green: #7BAE73 !default;
 
-$header-margin-bottom: 1rem;
+$header-pading: 1rem;
 $grid-gap: 1rem;
 
-.products {
+.layout {
 	color: $font-color-dafault;
+}
+
+
 	
+	.header {
+		padding: $header-pading 0;
+		
+		.container {
+			display: flex;
+			justify-content: space-between;
+		}
+		
+		.title {
+			font-size: 1.75rem;
+			font-weight: 600;
+			line-height: 1.25;
+			margin: 0;
+			// margin-bottom: 1rem;
+		}
+		
+		
+		@include breakpoint($breakpoint-xs) {
+			.sort {
+				display: none;
+			}
+		}
+	}
+	
+	.dropdown {
+			position: relative;
+			color: $color-grey;
+			// margin-bottom: 1rem;
+			
+			.dropdown-btn {
+				display: flex;
+				align-items: center;
+				align-self: flex-end;
+				font-family: $font-family-default, sans-serif;
+				font-size: 0.75rem;
+				line-height: 1.25;
+				color: inherit;
+				background-color: $color-white;
+				border: none;
+				border-radius: 0.25rem;
+				box-shadow: 0px 2px 5px rgb(0 0 0 / 10%);
+				// margin-bottom: 1rem;
+				padding: 0.625rem 1rem calc(0.625rem + 1px);
+				font-weight: 400;
+				cursor: pointer;
+				transition: color .2s ease;
+				
+				@include hover() {
+					color: $font-color-dafault;
+					
+					.dropdown-icon {
+						border-color: $font-color-dafault;
+					}
+				}
+			}
+		
+			.dropdown-icon {
+				position: relative;
+				top: -1px;
+				display: inline-block;
+				width: 5px;
+				height: 5px;
+				border-top: 1px solid $color-grey;
+				border-left: 1px solid $color-grey;
+				margin-left: 6px;
+				transform-origin: 50% 50%;
+				transform: rotate(225deg);
+				
+				transition: transform .2s ease, border-color .2s ease;
+			}
+			
+			.options {
+				position: absolute;
+				top: 80%;
+				right: 0;
+				display: block;
+				width: 100%;
+				max-width: 510px;
+				min-width: fit-content;
+				list-style-type: none;
+				border-radius: 4px;
+				background-color: $color-white;
+				box-shadow: 0px 20px 30px rgb(0 0 0 / 4%), 0px 6px 10px rgb(0 0 0 / 2%);
+				margin: 0;
+				padding: 0;
+				z-index: 2;
+				
+				visibility: hidden;
+				opacity: 0;
+				
+				transition: top .2s ease, opacity .2s ease, visibility .2s ease;
+			}
+		
+			.option {
+				color: inherit;
+				font-size: .825rem;
+				padding: 0.625rem 1rem;
+				cursor: pointer;
+				transition: color .2s ease;
+				
+				@include hover() {
+					color: $font-color-dafault;
+				}
+				
+				@media (hover: none) {
+					color: $font-color-dafault;
+				}
+			}
+			
+			&.opened {
+				.dropdown-btn {
+					color: $font-color-dafault;
+				}
+				
+				.dropdown-icon {
+					top: initial;
+					bottom: -1px;
+					border-color: $font-color-dafault;
+					transform: rotate(45deg);
+				}
+				
+				.options {
+					top: calc(100% + $header-pading);
+					opacity: 1;
+					visibility: visible;
+				}
+			}
+			
+			@include breakpoint($breakpoint-xs) {
+				.options {
+					width: 100%;
+				}
+			}
+	}
+		
+.products {
 	@keyframes newProduct {
 		from {
 			opacity: .2;
@@ -113,20 +276,14 @@ $grid-gap: 1rem;
 	}
 
 	.container {
-		display: grid;
-		display: -ms-grid;
-		grid-template-columns: repeat(4, 1fr);
-		column-gap: $grid-gap;
-		padding: $container-padding;
+		// display: grid;
+		// display: -ms-grid;
+		// grid-template-columns: repeat(4, 1fr);
+		// column-gap: $grid-gap;
+		// padding: $container-padding;
 	}
 	
-	.title {
-		font-size: 1.75rem;
-		font-weight: 600;
-		line-height: 1.25;
-		margin: 0;
-		margin-bottom: $header-margin-bottom;
-	}
+
 	
 	.form {
 		position: sticky;
@@ -137,115 +294,12 @@ $grid-gap: 1rem;
 		grid-column: 2 / 5;
 	}
 	
-	.filters {
-		grid-column: 1 / 4;
-		display: flex;
-		justify-content: flex-end;
+
+	.sort {
+		display: none;
+		margin-bottom: 1rem;
 	}
-	
-	.dropdown {
-		position: relative;
-		color: $color-grey;
-		
-		.dropdown-btn {
-			display: flex;
-			align-items: center;
-			align-self: flex-end;
-			font-family: $font-family-default, sans-serif;
-			font-size: 0.75rem;
-			line-height: 1.25;
-			color: inherit;
-			background-color: $color-white;
-			border: none;
-			border-radius: 0.25rem;
-			box-shadow: 0px 2px 5px rgb(0 0 0 / 10%);
-			margin-bottom: 1rem;
-			padding: 0.625rem 1rem calc(0.625rem + 1px);
-			font-weight: 400;
-			cursor: pointer;
-			transition: color .2s ease;
-			
-			@include hover() {
-				color: $font-color-dafault;
-				
-				.dropdown-icon {
-					border-color: $font-color-dafault;
-				}
-			}
-		}
-	
-		.dropdown-icon {
-			position: relative;
-			top: -1px;
-			display: inline-block;
-			width: 5px;
-			height: 5px;
-			border-top: 1px solid $color-grey;
-			border-left: 1px solid $color-grey;
-			margin-left: 6px;
-			transform-origin: 50% 50%;
-			transform: rotate(225deg);
-			
-			transition: transform .2s ease, border-color .2s ease;
-		}
-		
-		.options {
-			position: absolute;
-			top: 80%;
-			right: 0;
-			display: block;
-			width: 100%;
-			max-width: 510px;
-			min-width: fit-content;
-			list-style-type: none;
-			border-radius: 4px;
-			background-color: $color-white;
-			box-shadow: 0px 20px 30px rgb(0 0 0 / 4%), 0px 6px 10px rgb(0 0 0 / 2%);
-			margin: 0;
-			padding: 0;
-			z-index: 2;
-			
-			visibility: hidden;
-			opacity: 0;
-			
-			transition: top .2s ease, opacity .2s ease, visibility .2s ease;
-		}
-	
-		.option {
-			color: inherit;
-			font-size: .825rem;
-			padding: 0.625rem 1rem;
-			cursor: pointer;
-			transition: color .2s ease;
-			
-			@include hover() {
-				color: $font-color-dafault;
-			}
-			
-			@media (hover: none) {
-				color: $font-color-dafault;
-			}
-		}
-		
-		&.opened {
-			.dropdown-btn {
-				color: $font-color-dafault;
-			}
-			
-			.dropdown-icon {
-				top: initial;
-				bottom: -1px;
-				border-color: $font-color-dafault;
-				transform: rotate(45deg);
-			}
-			
-			.options {
-				top: 100%;
-				opacity: 1;
-				visibility: visible;
-			}
-		}
-	}
+
 	
 
 	.cards {
@@ -268,7 +322,7 @@ $grid-gap: 1rem;
 	@include breakpoint($breakpoint-sm) {
 		.container {
 			grid-template-columns: repeat(2, 1fr);
-			padding: $container-padding--mobile;
+			// padding: $container-padding--mobile;
 		}
 		
 		.content {
@@ -281,6 +335,12 @@ $grid-gap: 1rem;
 	}
 	
 	@include breakpoint($breakpoint-xs) {
+		.header {
+			
+			// .filters {
+			// 	display: none;
+			// }
+		}
 		.container {
 			grid-template-columns: 1fr;
 		}
@@ -295,16 +355,17 @@ $grid-gap: 1rem;
 			grid-column: initial;
 		}
 		
-		.filters {
+		.sort {
+			display: block;
 			position: sticky;
 			top: 1rem;
 			z-index: 2;
 		}
-		.dropdown {
-			.options {
-				width: calc(100vw - $container-padding--mobile * 2);
-			}
-		}
+		// .dropdown {
+		// 	.options {
+		// 		width: calc(100vw - $container-padding--mobile * 2);
+		// 	}
+		// }
 	}
 }
 </style>
